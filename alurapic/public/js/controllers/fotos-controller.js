77b86c7c35801a0c985b05b48a4cef35934d99f6
joaopaulo164 <joaopaulo164@hotmail.com>
2016-05-03 12:20:17 -0300

    Aula 2 finalizada

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -2,10 +2,24 @@
  * Created by Joao on 03/05/2016.
  */
 
-angular.module('alurapic').controller('FotosController', function($scope){ //injeção do scope para ligação entre o controller e a view
-    //dependurando foto no scope
-    $scope.foto = {
-        titulo: 'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+angular.module('alurapic').controller('FotosController', function($scope, $http){ //injeção do scope para ligação entre o controller e a view e http para realizar requisições AJAX
+
+    $scope.fotos = [];
+
+    $http.get('v1/fotos') //Atalho para promesse
+        .success(function(fotos){
+            $scope.fotos = fotos;
+        })
+        .error(function(error){
+            console.log(error);
+        });
+
+    /*
+    var promisse = $http.get('v1/fotos'); //Requisição ao servidor pelo padrão promisse
+    promisse.then(function(retorno){
+        $scope.fotos = retorno.data; //dependurando fotos no scope
+    }).catch(function(error){
+        console.log(error);
+    });*/
+
 });
